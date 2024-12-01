febfa402a07a809fdecd17d410f17ca2933c9190
Singhpriyanshu2907 <priyanshus2907@gmail.com>
2024-12-02 03:18:58 +0530

    CICD

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ except Exception as e:
 
 STAGE_NAME = "Evaluation stage"
 try:
-   logger.info(f"*******************")
+   logger.info(f"**********************")
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
    model_evalution = EvaluationPipeline()
    model_evalution.main()
